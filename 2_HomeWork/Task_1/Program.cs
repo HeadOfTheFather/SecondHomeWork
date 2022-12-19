75a91112c173c0d3d5fbd97c96a8227630b63759
Aleksandr Malikov <HeadOfTheFather@gmail.com>
2022-12-19 23:36:14 +0300

    Done

---
+++ 2_HomeWork/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Write a three-digit number");
+string n = Console.ReadLine();
+if (n.Length == 3)
+{
+    Console.WriteLine(n[n.Length - 2]);
+}
+else
+    Console.WriteLine("Error: Please write a three-digit number!");
---
+++ 2_HomeWork/Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Write a three-digit number");
+string n = Console.ReadLine();
+if (n.Length < 3)
+{
+    Console.WriteLine("Error: There is not a third sign");
+}
+else
+        Console.WriteLine(n[2]);
